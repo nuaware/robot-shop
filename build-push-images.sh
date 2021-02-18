4c6a080439e5ad845da1ce3c2fee419496de584f
Patrick van der Bleek <patrick@nuaware.com>
2021-02-18 11:39:10 +0100

    Ouch, forgot to remove a hash....

--- build-push-images.sh
+++ build-push-images.sh
@@ -4,7 +4,7 @@ for DFILE in $(find . -name Dockerfile -print)
 do
    SVC=`echo $DFILE | awk -F/ '{print $2}'`
    cd $SVC
-   #docker build -t nuaware/rs-${SVC}:latest . --platform linux/amd64
+   docker build -t nuaware/rs-${SVC}:latest . --platform linux/amd64
    docker push nuaware/rs-${SVC}:latest
    cd ..
 done
