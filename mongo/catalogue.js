a98fe0e5c37b6497e653dea4c560d4f8562bcf43
Patrick van der Bleek <patrick@nuaware.com>
2021-02-18 16:10:47 +0100

    Added new products

--- mongo/catalogue.js
+++ mongo/catalogue.js
@@ -3,17 +3,14 @@
 //
 db = db.getSiblingDB('catalogue');
 db.products.insertMany([
-    {sku: 'HAL-1', name: 'HAL', description: 'Sorry Dave, I cant do that', price: 2001, instock: 2, categories: ['Artificial Intelligence']},
-    {sku: 'PB-1', name: 'Positronic Brain', description: 'Highly advanced sentient processing unit with the laws of robotics burned in', price: 200, instock: 0, categories: ['Artificial Intelligence']},
-    {sku: 'ROB-1', name: 'Robbie', description: 'Large mechanical workhorse, crude but effective. Comes in handy when you are lost in space', price: 1200, instock: 12, categories: ['Robot']},
-    {sku: 'EVE-1', name: 'Eve', description: 'Extraterrestrial Vegetation Evaluator', price: 5000, instock: 10, categories: ['Robot']},
-    {sku: 'C3P0', name: 'C3P0', description: 'Protocol android', price: 953, instock: 1, categories: ['Robot']},
-    {sku: 'R2D2', name: 'R2D2', description: 'R2 maintenance robot and secret messenger. Help me Obi Wan', price: 1024, instock: 1, categories: ['Robot']},
-    {sku: 'K9', name: 'K9', description: 'Time travelling companion at heel', price: 300, instock: 12, categories: ['Robot']},
-    {sku: 'RD-10', name: 'Kryten', description: 'Red Drawf crew member', price: 700, instock: 5, categories: ['Robot']},
-    {sku: 'HHGTTG', name: 'Marvin', description: 'Marvin, your paranoid android. Brain the size of a planet', price: 42, instock: 48, categories: ['Robot']},
-    {sku: 'STAN-1', name: 'Stan', description: 'APM guru', price: 67, instock: 1000, categories: ['Robot', 'Artificial Intelligence']},
-    {sku: 'STNG', name: 'Mr Data', description: 'Could be R. Daneel Olivaw? Protype positronic brain android', price: 1000, instock: 0, categories: ['Robot']}
+    {sku: 'INS-1', name: 'Instana T-Shirt', description: 'Getting dressed for the occasion.', price: 15, instock: 2, categories: ['Swag']},
+    {sku: 'INS-2', name: 'Squeezy Stan', description: 'Your ultimate partner for stressful moments.', price: 5, instock: 0, categories: ['Swag']},
+    {sku: 'GRM-1', name: 'Gremlin Swag Pack', description: 'Unleash real-life chaos with the Gremlin mascotte.', price: 25, instock: 12, categories: ['Swag']},
+    {sku: 'HSC-1', name: 'HashiCorp Mug', description: 'Do not mess with me before coffee.', price: 5, instock: 10, categories: ['Swag']},
+    {sku: 'HSC-2', name: 'HashiCorp Socks', description: 'Almost as safe to store stuff in as a Vault.', price: 10, instock: 1, categories: ['Swag']},
+    {sku: 'K8S-1', name: 'Kubernetes Cap', description: 'The perfect headgear.', price: 20, instock: 1, categories: ['Swag']},
+    {sku: 'DCK-1', name: 'Docker Tie', description: 'The name is Dock, Moby Dock.', price: 15, instock: 12, categories: ['Swag']},
+    {sku: 'PSC-1', name: 'Prisma Cloud T-Shirt', description: 'Feeling secure yet?', price: 15, instock: 5, categories: ['Swag']}
 ]);
 
 // full text index for searching
@@ -27,4 +24,3 @@ db.products.createIndex(
     { sku: 1 },
     { unique: true }
 );
-
