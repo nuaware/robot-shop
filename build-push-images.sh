406cb33342c51b91b1a8b1196b32670a3bd93adb
Patrick van der Bleek <patrick@nuaware.com>
2021-02-18 11:38:40 +0100

    Added build script for custom images

---
+++ build-push-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for DFILE in $(find . -name Dockerfile -print)
+do
+   SVC=`echo $DFILE | awk -F/ '{print $2}'`
+   cd $SVC
+   #docker build -t nuaware/rs-${SVC}:latest . --platform linux/amd64
+   docker push nuaware/rs-${SVC}:latest
+   cd ..
+done
