ad9758f430231819a0e8b8101cd3b10536811ab9
Patrick van der Bleek <patrick@nuaware.com>
2021-02-23 11:38:59 +0100

    Added some products to the shop

--- mongo/catalogue.js
+++ mongo/catalogue.js
@@ -10,6 +10,11 @@ db.products.insertMany([
     {sku: 'HSC-2', name: 'HashiCorp Socks', description: 'Almost as safe to store stuff in as a Vault.', price: 10, instock: 1, categories: ['Swag']},
     {sku: 'K8S-1', name: 'Kubernetes Cap', description: 'The perfect headgear.', price: 20, instock: 1, categories: ['Swag']},
     {sku: 'DCK-1', name: 'Docker Tie', description: 'The name is Dock, Moby Dock.', price: 15, instock: 12, categories: ['Swag']},
+    {sku: 'NGX-1', name: 'NGINX T-Shirt', description: 'en-jin-eks.', price: 15, instock: 6, categories: ['Swag']},
+    {sku: 'EXN-1', name: 'EXN Calendar', description: 'Exclusive Networks 2021 Calendar.', price: 5, instock: 10, categories: ['Swag']},
+    {sku: 'GIT-1', name: 'Octocat', description: 'The official Github mascotte.', price: 15, instock: 12, categories: ['Swag']},
+
+
     {sku: 'PSC-1', name: 'Prisma Cloud T-Shirt', description: 'Feeling secure yet?', price: 15, instock: 5, categories: ['Swag']}
 ]);
 
